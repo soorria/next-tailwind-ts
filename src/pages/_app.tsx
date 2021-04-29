2accac38ceeef236a192665ef227a7b99ba42475
Soorria Saruva <soorria.ss@gmail.com>
2021-04-29 11:44:22 +1000

    initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import { AppProps } from 'next/app'
+import '../styles/globals.css'
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default MyApp
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+const HomePage: React.FC = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Head>
+        <title>Soorria&apos;s Next.js Template</title>
+      </Head>
+    </div>
+  )
+}
+
+export default HomePage
