62d332c61f3507397d7df647c5331a57dfc20706
Soorria Saruva <soorria.ss@gmail.com>
2021-04-29 12:13:07 +1000

    Update home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,13 @@ import Head from 'next/head'
 
 const HomePage: React.FC = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex flex-col min-h-screen px-4 py-8 bg-gray-100">
       <Head>
         <title>Soorria&apos;s Next.js Template</title>
       </Head>
+      <h1 className="mx-4 mb-16 text-3xl font-bold text-center sm:text-4xl md:text-5xl">
+        Soorria&apos;s Next.js + TailwindCSS + TypeScript Template
+      </h1>
     </div>
   )
 }
