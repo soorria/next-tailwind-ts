fa246b4b29de4a57e0abbae3198309af619ec522
Soorria Saruva <soorria.ss@gmail.com>
2021-07-02 13:46:54 +1000

    Separate eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,10 +25,9 @@ module.exports = {
     'next',
     'next/core-web-vitals',
     'plugin:jsx-a11y/recommended',
-    'plugin:prettier/recommended', // Make sure this is always the last element in the array.
+    'prettier',
   ],
   rules: {
-    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
     'react/react-in-jsx-scope': 'off',
     'react/prop-types': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
